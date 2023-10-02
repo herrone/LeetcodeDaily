abed6966109aac906ef676acb8a74c581340f018
herrone <egretfulherron@gmail.com>
2023-10-02 16:48:01 +0100

    Added twoSum sorted

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,24 @@ public class Main {
     public static void main(String[] args) {
 
     }
-
+     public int[] twoSumSorted(int[] numbers, int target) {
+       int low = 0;
+        int high = numbers.length-1;
+        while(low < high){
+            if(numbers[low] + numbers[high] == target){
+return new int [] {low + 1, high+1};
+             }
+            else if( numbers[low]  + numbers[high] > target){
+                high--;
+            }
+            else {
+                low++;
+            }
+                
+            }
+        
+    return new int [] { 1,2};
+    }
     public int frequency(String s){
 
         
